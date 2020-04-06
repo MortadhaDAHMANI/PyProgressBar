33b1ca439a867cecc1b9fd84c1d573d48b0d4c02
Mortadha DAHMANI <40170728+MortadhaDAHMANI@users.noreply.github.com>
2020-04-06 14:53:30 +0100

    Create progress-bar.py
    color progress-bar without library

---
+++ progress-bar.py
@@ -0,0 +1,44 @@
+import time
+import datetime
+import sys
+from time import sleep
+
+TGREEN = '\033[32m'   # Green Text
+TRED = '\033[31m'     # RED Text
+TBLACK = '\033[30m'   # BLACK Text
+TRESET = '\033[m'     # RESET Text
+
+
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 *(iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' %(prefix, TRED+bar+TRESET, percent, suffix), end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
+# A List of Items
+items = list(range(0, 57))
+l = len(items)
+
+# Initial call to print 0% progress
+printProgressBar(0, l, prefix='Progress:', suffix='Complete', length=50)
+for i, item in enumerate(items):
+    # Do stuff...
+    time.sleep(0.05)
+    # Update Progress Bar
+    printProgressBar(i + 1, l, prefix='Progress:', suffix='Complete', length=50)
